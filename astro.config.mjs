d146a0a893435ed77923bf3068ca4b706b752737
Noelia <noe.sanchezg@gmail.com>
2025-10-30 23:24:10 +0100

    nueva web con diseño más moderno

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,25 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.timecupleague.com',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   build: {
     inlineStylesheets: 'auto',
   },
+
   compressHTML: true,
+
   image: {
     domains: ['timecupleague.com'],
   },
+
+  integrations: [react()],
 })
