e0ba56e4b272e2b26385602d6eeeffce82b61083
Noelia <noe.sanchezg@gmail.com>
2025-11-24 17:17:48 +0100

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 import react from '@astrojs/react'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
